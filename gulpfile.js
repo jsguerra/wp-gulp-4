62fc7de154ad0ff0f845b1c7b8d858b7ced7fe79
Jose Guerra <jose@gcostudios.com>
2019-06-13 20:18:57 +0100

    added initial basic setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,36 @@
+const themeName = 'theme-name';
 
+// Set variables
+const { src, dest, watch, series, parallel } = require('gulp'),
+      autoprefixer = require('autoprefixer'),
+      browserSync = require('browser-sync').create(),
+      postcss = require('gulp-postcss'),
+      sass = require('gulp-sass'),
+      sourcemaps = require('gulp-sourcemaps');
+
+// Path variables
+const srcFolder = '../' + themeName + '/',
+      scssFolder = srcFolder + 'sass/',
+      jsFolder = srcFolder + 'js/';
+
+function scssTask() {
+  return src(scssFolder + '{style.scss,rtl.scss}')
+    .pipe(sourcemaps.init())
+    .pipe(sass()
+    .on('error', sass.logError))
+    .pipe(postcss([autoprefixer]))
+    .pipe(sourcemaps.write())
+    .pipe(dest(srcFolder)
+  );
+}
+
+function jsTask() {
+  return src([jsFolder + '*.js'])
+    .pipe(dest(jsFolder));
+}
+
+function watchTask() {
+  watch([scssFolder + '{style.scss,rtl.scss}', jsFolder + '*.js'], parallel(scssTask, jsTask));
+}
+
+exports.default = series(watchTask);
